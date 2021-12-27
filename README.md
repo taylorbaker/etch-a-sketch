c52e8bf96a27f805ca3d98dde18a65153d1493a2
taylorbaker <tdbaker2@asu.edu>
2021-12-27 16:40:12 -0700

    Implement basic logic and operations

---
+++ README.md
@@ -0,0 +1,5 @@
+# Etch-A-Sketch
+
+This project is designed to demonstrate DOM manipulation JS principles and CSS stylings according to the [TOP etch-a-sketch][1] project.
+
+[1]: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project
---
+++ script.js
@@ -0,0 +1,47 @@
+const display = document.getElementById('display');
+const input = document.getElementById('input');
+
+// get matrix dims from user with input
+let DIM = parseInt(input.value, 10); // base=10
+
+// helper function to create a row of divs
+function createRow(count) {
+  const newRow = document.createElement('div');
+  newRow.classList.add('row');
+  for (let i = 0; i < count; i++) {
+    const node = document.createElement('div');
+    node.classList.add('node');
+    newRow.appendChild(node);
+  }
+  return newRow;
+}
+
+// create desired number of rows
+function createMatrix(count) {
+  for (let i = 0; i < count; i++) {
+    let newRow = createRow(count);
+    display.appendChild(newRow);
+  }
+}
+
+// button logic
+const btn = document.getElementById('button');
+btn.addEventListener('click', () => {
+  removeAllChildNodes(display);
+
+  // cap DIM at 100
+  if (input.value >= 100) {
+    DIM = 100;
+  } else {
+    DIM = parseInt(input.value, 10);
+  }
+
+  createMatrix(DIM);
+});
+
+// function to empty parent node
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
